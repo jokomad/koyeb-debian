30e2376ad33d0922f1a004ee058097ee16a44b30
jokomad <83660420+jokomad@users.noreply.github.com>
2023-12-06 22:17:45 +0100

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -2,5 +2,4 @@
 #exec mkdir -p /tmp/1;
 #exec tar -xf /1.tar.gz -C /tmp/1
 #exec chromium --renderer-process-limit=2  --origin-trial-disabled-features=WebGPU --disable-partial-raster --disable-mipmap-generation --render-process-limit=1 --disable-zero-copy --media-router=0 --disable-pings --no-default-browser-check --debug-devtools-frontend=0 --render-process-limit=1 --disable-zero-copy --disable-partial-raster --disable-mipmap-generation --memory-model=low --disable-features=GlobalMediaControls --disable-browser-side-navigation --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --no-sandbox --user-data-dir=/home/j/1 --disable-site-isolation-trials --enable-low-end-device-mode --renderer-process-limit=1
-exec chromium --disable-dev-shm-usage --no-sandbox
-
+exec chromium --no-sandbox --disable-background-networking --disable-default-apps	--disable-extensions --disable-sync --disable-translate	--hide-scrollbars --metrics-recording-only --mute-audio --no-first-run --safebrowsing-disable-auto-update --ignore-certificate-errors --ignore-ssl-errors --ignore-certificate-errors-spki-list
